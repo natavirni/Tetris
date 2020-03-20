c0b038c1bcd928eab93661063a153428ae586a73
Natashia Virnilia <61217073+natavirni@users.noreply.github.com>
2020-03-20 18:55:25 +0700

    Extract Method

--- src/source/WindowFrame.java
+++ src/source/WindowFrame.java
@@ -5,22 +5,29 @@ import javax.swing.JFrame;
 public class WindowFrame extends JFrame {
 
 	public static final int WIDTH = 640, HEIGHT = 720;
-
-	public WindowFrame() {
-		setTitle("Game Legendaris Klasik Susun Balok Lucu Warna-Warni Hanya Menyerupai tapi Bukan TETRIS 2019");
-
+	
+	public UntuksetSizeWindowFrame(){
 		setSize(WIDTH, HEIGHT);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setResizable(false);
+	}
+
+	public UntuksetGlassPaneWindowFrame(){
+		setGlassPane(new GlassPane());
+		getGlassPane().setVisible(true);
+		addKeyListener(board);
+	}
+
+	public WindowFrame() {
+		setTitle("Game Legendaris Klasik Susun Balok Lucu Warna-Warni Hanya Menyerupai tapi Bukan TETRIS 2019");
+
+		UnutksetSizeWindowFrame();
 
 		Board board = new Board();
 		add(board);
 
-		setGlassPane(new GlassPane());
-		getGlassPane().setVisible(true);
-		addKeyListener(board);
-
+		UntuksetGlassPaneWindowFrame();
 		setVisible(true);
 	}
 
